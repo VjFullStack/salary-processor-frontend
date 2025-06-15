55d8a90efa55a8d2882d36e5e46f580412455cf3
VjFullStack <57318666+VjFullStack@users.noreply.github.com>
2025-06-15 13:02:44 +0530

    Fix React Query v5 syntax in useEmployeeData hook

--- src/hooks/useEmployeeData.js
+++ src/hooks/useEmployeeData.js
@@ -22,13 +22,15 @@ export const useEmployeeData = () => {
   };
 
   // Use React Query to fetch and cache data
-  return useQuery(['employeeData'], fetchEmployeeData, {
+  return useQuery({
+    queryKey: ['employeeData'],
+    queryFn: fetchEmployeeData,
     // Don't refetch on window focus
     refetchOnWindowFocus: false,
     // Cache will be considered fresh for 5 minutes
     staleTime: 5 * 60 * 1000, 
     // Data will be kept in cache for 30 minutes even when unused
-    cacheTime: 30 * 60 * 1000,
+    gcTime: 30 * 60 * 1000, // Note: cacheTime was renamed to gcTime in v5
     // Don't fetch data if there's no token
     enabled: !!token,
   });
